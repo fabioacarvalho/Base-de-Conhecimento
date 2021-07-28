40c7dfd6358f0d75a138d53eb2dcf8a8b36d2817
Fábio Carvalho <fabioxcarvalho@outlook.com>
2021-07-28 12:58:15 -0300

    Create ArticleById.vue

---
+++ versao-inicial/frontend/src/components/article/ArticleById.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import { baseApiUrl } from '@/global.js'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle.vue'
+
+export default {
+    name: 'ArticleById',
+    components: { PageTitle },
+    data: function() {
+        return {
+            article: {}
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
